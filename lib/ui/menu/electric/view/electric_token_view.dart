7ee068b9c2afb6f485d8a7ba2ce4470596548d64
chairilraffi <chairilraffi11@gmail.com>
2022-02-27 16:53:29 +0700

    electric token --electric token layout

---
+++ lib/ui/menu/electric/view/electric_token_view.dart
@@ -0,0 +1,116 @@
+import 'package:benpay/core/benpay_palette.dart';
+import 'package:benpay/core/util/constant.dart';
+import 'package:benpay/ui/component/component.dart';
+import 'package:benpay/ui/payment/view/payment_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:nav_router/nav_router.dart';
+
+class ElectricToken extends StatelessWidget {
+  const ElectricToken({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    const double itemHeight = 120;
+    final double itemWidth = size.width / 1.8;
+    return Scaffold(
+      body: Stack(
+        children: [
+          Component.header(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                Component.appBar("Listrik Token", transparet: true),
+                Card(
+                  margin: const EdgeInsets.symmetric(vertical: 10),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+                    child: TextFormField(
+                      // controller: phoneContactController,
+                      decoration: Component.inputDecoration("No Handpone"),
+                      maxLength: 16,
+                      keyboardType: TextInputType.number,
+                      inputFormatters: [
+                        FilteringTextInputFormatter.digitsOnly,
+                      ],
+                      validator: (value) {
+                        if (value?.isEmpty ?? true) {
+                          return "Wajib diisi*";
+                        }
+                      },
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10,),
+                Component.textBold("Pilih Token"),
+                const SizedBox(height: 10,),
+                Flexible(
+                  child: GridView.builder(
+                    // physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    padding: const EdgeInsets.all(0),
+                    itemCount: 20,
+                    scrollDirection: Axis.vertical,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: (itemWidth / itemHeight),
+                    ),
+                    itemBuilder: (BuildContext context, int index) { 
+                      return InkWell(
+                        onTap: () => routePush(PaymentView(), RouterType.material),
+                        child: Card(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Component.textBold(
+                                  "$index.000", 
+                                  fontSize: BenpayConstant.fontSizeLargeExtra
+                                ),
+                                const SizedBox(height: 10,),
+                                Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Component.textBold(
+                                      "RP 30.000",
+                                      fontSize: BenpayConstant.fontSizeMedium,
+                                      colors: BenpayPalette.orange
+                                    ),
+                                    const SizedBox(width: 10,),
+                                    const Text(
+                                      "RP 35.000",
+                                      style: TextStyle(
+                                        color: BenpayPalette.grey,
+                                        fontFamily: BenpayConstant.avenirRegular,
+                                        fontSize: BenpayConstant.fontSizeSmall,
+                                        overflow: TextOverflow.ellipsis,
+                                        decoration: TextDecoration.lineThrough
+                                      ),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ); 
+  }
+}
