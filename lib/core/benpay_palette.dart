6a1c01862a27331e2f597de28d6191d4da1e9f0b
@radityadean <radityadean91@gmail.com>
2022-02-28 06:53:07 +0700

    add component from branch develop

--- lib/core/benpay_palette.dart
+++ lib/core/benpay_palette.dart
@@ -10,47 +10,4 @@ class BenpayPalette {
   // static const Color greenBackground = Color(0xffd8eada);
   static const Color white = Color(0xFFFFFFFF);
   static const Color grey = Color(0xFFBDBDBD);
-
-  // static const MaterialColor greenMaterial = MaterialColor(0xFF3E9543);
-  static const Color greenDark = Color(0xFF377C33);
-  static const Color greenBackground = Color(0xffd8eada);
-  static const Color whiteBackground = Color(0xffFAFAFA);
-  static const Color transparent = Color(0xFF737373);
-  static const Color yellow = Color(0xFFFDCA00);
-  static const Color orange = Color(0xFFF8A700);
-  static const Color red = Color(0xFFdc1616);
-  static const Color grey200 = Color.fromARGB(50, 50, 50, 0);
-  static const Color gold = Color.fromARGB(255, 255, 215, 0);
-  static const Color blackText = Color(0xff25282B);
-  static const Color black = Color(0xDD000000);
-  static const Color blacktransparent = Color(0x0c737373);
-
-  static const Color text = Color(0xff797979);
-  static const Color icon = Color(0x8F404040);
-
-  //? Fade Black
-  static const Color overlay_0 = Color(0x00000000);
-  static const Color overlay_10 = Color(0x1A000000);
-  static const Color overlay_20 = Color(0x33000000);
-  static const Color overlay_30 = Color(0x4D000000);
-  static const Color overlay_40 = Color(0x66000000);
-  static const Color overlay_50 = Color(0x80000000);
-  static const Color overlay_60 = Color(0x99000000);
-  static const Color overlay_70 = Color(0xB3000000);
-  static const Color overlay_80 = Color(0xCC000000);
-  static const Color overlay_90 = Color(0xE6000000);
-  static const Color overlay_100 = Color(0xFF000000);
-
-  //status balance
-  static const Color pulsaStatus1 = Color(0xff08d34c);
-  static const Color pulsaStatus2 = Color(0xffffd700);
-  static const Color pulsaStatus3 = Color(0xffff6666);
-
-  static const Color successStatus = Color(0xFFD3FFE1);
-  static const Color pendingStatus = Color(0xFFFFFB95);
-  static const Color failedStatus = Color(0xFFD8D8D8);
-
-  static const Color successStatusText = Color(0xFF00B33E);
-  static const Color pendingStatusText = Color(0xFF8B872A);
-  static const Color failedStatusText = Color(0xFF606060);
 }
--- lib/ui/login/view/login.dart
+++ lib/ui/login/view/login.dart
@@ -1,8 +1,7 @@
 import 'package:benpay/core/benpay_palette.dart';
 import 'package:benpay/core/util/size_config.dart';
-import 'package:benpay/ui/component/component.dart';
 import 'package:benpay/ui/dashboard/view/dashboard.dart';
-import 'package:benpay/ui/register/view/register.dart';
+import 'package:benpay/ui/register/view/register_form.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:nav_router/nav_router.dart';
