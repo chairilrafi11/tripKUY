68c9780d9b10eb69f0ab66057c34b32343ef993c
chairilraffi <chairilraffi11@gmail.com>
2022-03-07 17:31:51 +0700

    profile --menu profile

--- lib/ui/profile/bloc/profile_menu_bloc.dart
+++ lib/ui/profile/bloc/profile_menu_bloc.dart
@@ -1,22 +1,25 @@
 import 'package:nav_router/nav_router.dart';
+import 'package:pintupay/ui/about_app/view/about_app.dart';
 import 'package:pintupay/ui/profile/bloc/profile_bloc.dart';
 
-import '../../login/view/login.dart';
-
 class ProfileMenuBloc {
+
   void navFeature(ProfileMenu profileMenu) {
     switch (profileMenu) {
-      case ProfileMenu.TERSM_CONDITION:
+      case ProfileMenu.info:
+        routePush(const AboutApp(), RouterType.material);
+        break;
+      case ProfileMenu.termCondition:
         // routePush(const Login(), RouterType.cupertino);
         break;
-      case ProfileMenu.CUSTOMER_SERVICE:
+      case ProfileMenu.cs:
         // routePush(const Login(), RouterType.cupertino);
         break;
-      case ProfileMenu.HOW_TO:
-        // routePush(const Login(), RouterType.cupertino);
+      case ProfileMenu.rate:
+        // TODO: Handle this case.
         break;
-      case ProfileMenu.ADDRESS:
-        // routePush(const Login(), RouterType.cupertino);
+      case ProfileMenu.password:
+        // TODO: Handle this case.
         break;
     }
   }
