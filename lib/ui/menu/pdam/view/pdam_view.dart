9621f71e623bdf590c0ae9e8f2ed839c55ea50b8
@radityadean <radityadean91@gmail.com>
2022-03-15 00:03:19 +0700

    validate pdam id customer

--- lib/ui/menu/pdam/view/pdam_view.dart
+++ lib/ui/menu/pdam/view/pdam_view.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:pintupay/ui/menu/pdam/model/pdam_disctric_response.dart';
 
+import '../../../../core/util/core_function.dart';
 import '../../../../core/util/size_config.dart';
 import '../cubit/pdam_cubit.dart';
 
@@ -29,65 +30,72 @@ class PDAMView extends StatelessWidget {
             child: ListView(
               children: [
                 Component.appBar("PDAM", transparet: true),
-                Card(
-                  margin: const EdgeInsets.symmetric(vertical: 10),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 30, horizontal: 20),
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          controller: idController,
-                          decoration:
-                              Component.decorationNoBorder("No Pelanggan"),
-                          maxLength: 16,
-                          keyboardType: TextInputType.number,
-                          inputFormatters: [
-                            FilteringTextInputFormatter.digitsOnly,
-                          ],
-                          validator: (value) {
-                            if (value?.isEmpty ?? true) {
-                              return "Wajib diisi*";
-                            }
-                          },
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        BlocProvider(
-                          create: (context) => pdamCubit,
-                          child: districtList(),
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Container(
-                          alignment: Alignment.center,
-                          margin: const EdgeInsets.symmetric(vertical: 25),
-                          child: InkWell(
-                            onTap: () {
-                              pdamCubit.onInquiry(idController.text);
+                Form(
+                  key: formKey,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  child: Card(
+                    margin: const EdgeInsets.symmetric(vertical: 10),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 30, horizontal: 20),
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            controller: idController,
+                            decoration:
+                                Component.decorationNoBorder("No Pelanggan"),
+                            maxLength: 16,
+                            keyboardType: TextInputType.number,
+                            inputFormatters: [
+                              FilteringTextInputFormatter.digitsOnly,
+                            ],
+                            validator: (value) {
+                              if (value?.isEmpty ?? true) {
+                                return "Wajib diisi*";
+                              }
                             },
-                            child: Container(
-                              width: SizeConfig.blockSizeHorizontal * 100,
-                              alignment: Alignment.center,
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 10, vertical: 10),
-                              decoration: const BoxDecoration(
-                                  color: PintuPayPalette.darkBlue,
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(10.0))),
-                              child: Component.textBold(
-                                'Tagihan',
-                                colors: PintuPayPalette.white,
-                                fontSize: 15,
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          BlocProvider(
+                            create: (context) => pdamCubit,
+                            child: districtList(),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Container(
+                            alignment: Alignment.center,
+                            margin: const EdgeInsets.symmetric(vertical: 25),
+                            child: InkWell(
+                              onTap: () {
+                                // pdamCubit.onInquiry(idController.text);
+                                if(formKey.currentState!.validate()){
+                                  pdamCubit.onInquiry(idController.text);
+                                }
+                              },
+                              child: Container(
+                                width: SizeConfig.blockSizeHorizontal * 100,
+                                alignment: Alignment.center,
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 10, vertical: 10),
+                                decoration: const BoxDecoration(
+                                    color: PintuPayPalette.darkBlue,
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(10.0))),
+                                child: Component.textBold(
+                                  'Tagihan',
+                                  colors: PintuPayPalette.white,
+                                  fontSize: 15,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 ),
